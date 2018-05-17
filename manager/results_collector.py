3d8b7b703f2685131259288e0208cdc2b531bb61
akrasuski <adam.p.krasuski@gmail.com>
2018-05-17 18:15:20 +0200

    result collector fixed

--- manager/results_collector.py
+++ manager/results_collector.py
@@ -10,7 +10,7 @@ import traceback
 
 try:
     class ResultsCollector():
-        def __init__(self, host, json_file, sim_id, floor):# {{{
+        def __init__(self, host, meta_file, sim_id):# {{{
             '''
             1. aamksrun makes gearman pass these jobs to workers: 
                 /usr/local/aamks/tests/worker.py
@@ -20,38 +20,44 @@ try:
                 * download animation.zip to workers/123/anim.zip
             '''
             self.host=host
-            self.json_file=json_file
+            self.meta_file=meta_file
             self.sim_id=sim_id
-            self.floor=floor
+            self.meta = None
 
-            self._dest_dir="{}/workers/{}".format(os.environ['AAMKS_PROJECT'], self.sim_id)
-            self._dest_json="{}/report.json".format(self._dest_dir)
             self.json=Json()
-            self._results_json()
+            self._fetch_meta()
             self._animation()
 # }}}
-        def _results_json(self):# {{{
-            Popen(["scp", "{}:{}".format(self.host, self.json_file), self._dest_json]).wait()
+        def _fetch_meta(self):# {{{
+            SendMessage("self.meta copied")
+            try:
+                Popen(["scp", "{}:{}".format(self.host, self.meta_file), self.meta_file]).wait()
+            except Exception as e:
+                SendMessage(e)
+            else: 
+                pass
+
+            self.meta=self.json.read(self.meta_file)
 # }}}
         def _fire_origin_coords(self, sim_id):# {{{
-            data=self.json.read("{}/workers/{}/evac.json".format(os.environ['AAMKS_PROJECT'], sim_id))
-            room=data['ROOM_OF_FIRE_ORIGIN']
+            room=self.meta['fire_origin']
             
-            self.s=Sqlite("{}/aamks.sqlite".format(os.environ['AAMKS_PROJECT']))
+            self.s=Sqlite("{}/aamks.sqlite".format(self.meta['path_to_project']))
             z=self.s.query("SELECT center_x, center_y FROM aamks_geom WHERE name=?", (room,))[0]
             return (z['center_x'], z['center_y'])
 # }}}
         def _animation(self):# {{{
-            data=self.json.read(self._dest_json)
-            Popen(["scp", "{}:{}".format(self.host, data['animation']), "{}/anim.zip".format(self._dest_dir)])
+            source = self.host+':'+self.meta['path_to_project']+'workers/'+str(self.meta['sim_id'])+'/'+self.meta['animation']
+            dest = self.meta['path_to_project']+'workers/'+str(self.meta['sim_id'])+'/'+self.meta['animation']
+            Popen(["scp", source, dest])
 
             self.jsonOut=OrderedDict()
             self.jsonOut['sort_id']=int(sim_id)
-            self.jsonOut['title']="sim{}, f{}".format(data['sim_id'], self.floor)
-            self.jsonOut['floor']=self.floor
-            self.jsonOut['fire_origin']=self._fire_origin_coords(data['sim_id'])
+            self.jsonOut['title']="sim{}, f{}".format(self.meta['sim_id'], self.meta['floor'])
+            self.jsonOut['floor']=self.meta['floor']
+            self.jsonOut['fire_origin']=self._fire_origin_coords(self.meta['sim_id'])
             self.jsonOut['highlight_geom']=None
-            self.jsonOut['anim']="{}".format(data['sim_id'])
+            self.jsonOut['anim']="{}".format(self.meta['sim_id'])
 
             anims_master="{}/workers/vis/anims.json".format(os.environ['AAMKS_PROJECT'])
             try:
@@ -63,21 +69,19 @@ try:
 # }}}
 
     try:
-        SendMessage(sys.argv)
         host=sys.argv[1]
-        json_file=sys.argv[2]
+        meta_file=sys.argv[2]
         sim_id=sys.argv[3]
-        floor=sys.argv[4]
     except:
         ''' Testing without gearman. Make sure workers/1/ exist and contains evac and cfast jsons. '''
         j=Json()
         c=j.read("{}/conf_aamks.json".format(os.environ['AAMKS_PROJECT']))
         host="localhost"
-        json_file="/home/aamks/{}/report_1.json".format(c['PROJECT_NAME'])
+        meta_file="/home/aamks/{}/report_1.json".format(c['PROJECT_NAME'])
         sim_id="1"
         floor="1"
 
-    ResultsCollector(host,json_file,sim_id,floor)
+    ResultsCollector(host,meta_file,sim_id)
 
 except:
     t="In results_collector.py:\n"
